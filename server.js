efdd14761b0dacadebd4a357f514dbf8cee9076b
yashcoding07 <yash07coding@gmail.com>
2025-09-12 12:09:05 +0530

    intialization of the project

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require("./src/app");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+
+httpServer.listen(3000, ()=>{
+    console.log("server is running on port 3000");
+});
---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.status(201).send("hello world");
+});
+
+
+module.exports = app;
